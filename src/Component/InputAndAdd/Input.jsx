5668449c440629badbb48f920268a5282ce9144f
deepak-2334 <deepak.singh@remotestate.com>
2024-10-25 00:47:50 +0530

    added todo on enter keyword press

--- src/Component/InputAndAdd/Input.jsx
+++ src/Component/InputAndAdd/Input.jsx
@@ -5,10 +5,24 @@ export  const  Input=({AddToDo})=> {
 
     const [input, setInput] = useState('')
 
+
+  const clickAddButton = () => {
+    AddToDo(input)
+    setInput("")
+   }
+
+   const clickEnterButton = (e) => {
+      console.log(e.key)
+      if (e.key === 'Enter') {
+        AddToDo(input)
+        setInput("")
+      } 
+   }
+
   return (
     <div className='input-button'>
-        <input value={input} type="text" placeholder='Enter your todo...' onChange={(e)=>{setInput(e.target.value)}}/>
-        <button className='add-button' onClick={()=>{AddToDo(input); setInput("")}} > add </button>
+        <input value={input} type="text" placeholder='Enter your todo...' onChange={(e)=>{setInput(e.target.value)}}  onKeyDown= {(e) => clickEnterButton(e)}/>
+        <button className='add-button' onClick={clickAddButton} > add </button>
     </div>
   )
 }
